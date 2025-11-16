eef47c14210127c3b2372addcadcded58d713a18
biemic <biemic@interia.pl>
2025-11-16 19:30:06 +0100

    Create Kurs_CodeGym.java

---
+++ src/First_program/Kurs_CodeGym.java
@@ -0,0 +1,13 @@
+package First_program;
+
+public class Kurs_CodeGym {
+
+    static void main(String[] args) {
+                                                         // skrót do static void main(String[] args) - psv
+
+        System.out.println("Siemka co tam ??");         // skrót do System.out.println(); - sout
+
+
+
+    }
+}
