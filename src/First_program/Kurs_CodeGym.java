31d68581fde51f1342b60099d913a8cc48dae244
biemic <biemic@interia.pl>
2025-11-16 19:51:45 +0100

    Update Kurs_CodeGym.java

--- src/First_program/Kurs_CodeGym.java
+++ src/First_program/Kurs_CodeGym.java
@@ -3,7 +3,7 @@ package First_program;
 public class Kurs_CodeGym {
 
     static void main(String[] args) {
-        // skrót do static void main(String[] args) - psv
+                                                         // skrót do static void main(String[] args) - psv
 
         System.out.println("Siemka co tam ??");         // skrót do System.out.println(); - sout
 
