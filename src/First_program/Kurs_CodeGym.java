a20cf0fcf672c83dd5b810f46de783856fd94af5
Michał Bieniasz <biemic@interia.pl>
2025-11-17 20:52:41 +0100

    Update Kurs_CodeGym.java

--- src/First_program/Kurs_CodeGym.java
+++ src/First_program/Kurs_CodeGym.java
@@ -5,7 +5,7 @@ public class Kurs_CodeGym {
     static void main(String[] args) {
                                                          // skrót do static void main(String[] args) - psv
 
-        System.out.println("Siemka co tam ??");         // skrót do System.out.println(); - sout
+        System.out.println("Siemka co tam ??");         // skrót do System.out.println(); - soutt
 
 
 
