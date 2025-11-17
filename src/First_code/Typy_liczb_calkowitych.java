97dcd7bb66541e569ca996730e9cb3653ff35e2d
biemic <biemic@interia.pl>
2025-11-17 16:48:17 +0100

    Create Typy_liczb_calkowitych.java

---
+++ src/First_code/Typy_liczb_calkowitych.java
@@ -0,0 +1,21 @@
+package First_code;
+
+public class Typy_liczb_calkowitych {
+
+
+
+/*  byte        1       Byte, ponieważ zajmuje jeden bajt pamięci
+    short       2       Skrót od Short Integer
+    int         4       Skrót od Integer
+    long        8       Skrót od Long Integer
+
+    zmiennoprzecinkowe
+
+    float       4        	Skrót od Floating Point Number
+    double      8           Skrót od Double Float
+ */
+
+
+
+
+}
